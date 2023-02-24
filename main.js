1f3ccf1a5a4b5053493cbbf9838116cdde364c7a
Alex Matte <alex.matte71@gmail.com>
2023-02-24 18:00:00 +0100

    created playerFactory function and beginning of the board module

---
+++ main.js
@@ -0,0 +1,22 @@
+const playerFactory = (name,id,mark) => {
+    return {name, id, mark}
+};
+
+const boardModule = (function(){
+    const displayController = "";
+    const checkForWinner = "";
+    const gameBoard = {
+        gameBoard: [],
+        player1: playerFactory("Alex", 1, "X"),
+        player2: playerFactory("Madi", 2, "O"),
+    }
+
+})();
+
+
+
+
+
+
+
+
