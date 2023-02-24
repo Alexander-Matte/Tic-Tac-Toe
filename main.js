ed72c694362d3ec5f545652738b37a1f95b6faf6
Alex Matte <alex.matte71@gmail.com>
2023-02-24 18:10:46 +0100

    cleaned up basic skeleton for app

--- main.js
+++ main.js
@@ -1,15 +1,23 @@
+//factory function to create players
 const playerFactory = (name,id,mark) => {
     return {name, id, mark}
 };
 
-const boardModule = (function(){
-    const displayController = "";
-    const checkForWinner = "";
+const gameModule = (function(){
+    //logic to check that after a move, if the game is over
+    const checkGameOver = () => console.log("game over");
+    //gameboard logic
     const gameBoard = {
         gameBoard: [],
         player1: playerFactory("Alex", 1, "X"),
         player2: playerFactory("Madi", 2, "O"),
+
     }
+    
+    //this will initialize and update the UI board
+    const displayController = {};
+    //controlls flow of game and keeps track of game data such as whos move
+    const gameController = {};
 
 })();
 
